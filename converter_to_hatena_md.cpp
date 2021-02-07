a3dbad0382b14a9857a42b911a42eca5b8caaad8
Qian-Donglin <sen.waseda.rikoten@gmail.com>
2021-02-08 00:29:15 +0900

    ひとまずの完成　　数式に関しては拡張ナシならOK

--- converter_to_hatena_md.cpp
+++ converter_to_hatena_md.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 /*
 Katexを使うことを前提とする。
-_, *, [, ], (, ) の6種類の文字に先頭に\とつけてエスケープする。
+*, [, ], (, ) の6種類の文字に先頭に\とつけてエスケープする。
 */
 
 //$$であるかを識別する
@@ -67,7 +67,7 @@ int main()
 					i++;
 					in_one_line = true;
 				}
-				if (is_in_the_text_delimiter(buf, i))
+				else if (is_in_the_text_delimiter(buf, i))
 				{
 					//文中モードの数式のスタート
 					in_the_text = true;
@@ -80,24 +80,48 @@ int main()
 				//数式モードの終わりか？
 				if (in_one_line && is_one_line_delimiter(buf, i))
 				{
+					//数式モードの終わりには空白を挟む　無駄なはてなリンクワードを防ぐため
+					ofs << ' ';
 					ofs << buf[i];
 					i++;
 					in_one_line = false;
 				}
 				else if (in_the_text && is_in_the_text_delimiter(buf, i))
 				{
+					//数式モードの終わりには空白を挟む　無駄なはてなリンクワードを防ぐため
+					ofs << ' ';
 					in_the_text = false;
 				}
 				else
 				{
 					//文中にある_, *, [, ], (, )はエスケープシーケンスをつける
-					if (buf[i] == '_' || buf[i] == '*' || buf[i] == '[' || buf[i] == ']' || buf[i] == '(' || buf[i] == ')')
+					if (buf[i] == '*' || buf[i] == '[' || buf[i] == ']' || buf[i] == '(' || buf[i] == ')')
 					{
 						ofs << '\\';
 					}
+
+					//文中にある_, ^は前後に半角空白を挟む
+					if (buf[i] == '_' || buf[i] == '^')
+					{
+						ofs << ' ';
+					}
 				}
 			}
+
 			ofs << buf[i];
+
+			//数式モードの_や^の両隣りには半角空白を挟む
+			if ((in_one_line || in_the_text) && (buf[i] == '_' || buf[i] == '^'))
+			{
+				ofs << ' ';
+			}
+
+			//数式モードを始めたら、無駄なはてなワードリンクを防ぐために1つ半角空白を空ける
+			if ((in_one_line || in_the_text) && (buf[i] == '$'))
+			{
+				//数式モードの始まりは半角空白を挟む　はてなワードでのリンクを防ぐため
+				ofs << ' ';
+			}
 		}
 		//改行
 		ofs << endl;
